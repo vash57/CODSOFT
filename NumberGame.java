e16f36050f4cd974a2de040d34fdb3ed6930afba
vash57 <devchaubey03@gmail.com>
2025-08-26 17:37:16 +0530

    Task1_NumberGuessingGame
    Number Game
    
    The Number Game is a simple and interactive console-based Java program where the computer generates a random number between 1 and 100, and the player has to guess it correctly within a limited number of attempts.
    
    Features
    
    Random number generation using Java’s Random class
    
    Console-based user interaction with clear instructions
    
    Tracks the number of attempts made by the player
    
    Option to exit the game anytime by entering -1
    
    Beginner-friendly and easy to run
    
    How It Works
    
    The program generates a secret number between 1 and 100.
    
    The player enters a guess.
    
    The program provides hints:
    
    "Too High" if the guess is greater than the secret number.
    
    "Too Low" if the guess is less than the secret number.
    
    The player continues guessing until they find the correct number or choose to quit.
    
    At the end, the program shows the total number of attempts used.
    
    Technology Used
    
    Language: Java
    
    Key Concepts: Random, Scanner, loops, conditionals

---
+++ NumberGame.java
@@ -0,0 +1,62 @@
+/*
+* PROJECT: NUMBER GAME
+* Internship:CODSOFT
+* Task: 1
+* Description: 
+* This program is a simple Number Guessing Game.
+* The Computer generates a random number,and the user
+* tries to guess it within a limited number of attempts
+
+* Author: DEV KUMAR CHAUBEY
+* Date : 26 Aug 2025
+*/
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("********* Number Game *******");
+        System.out.println("Defeat Me by Giving a no guess between 1 to 100");
+        System.out.println("And if you not interest then type -1 )");
+
+        int target = 1 + rand.nextInt(100); // To Generate the Random no
+        int attempts = 0;
+
+        // For Hints Part for Player
+        int low = 1;
+        int high = 100;
+
+        while (true) {
+            System.out.print("Enter Your Guess Number ");
+            int guess = sc.nextInt(); // Accept the Value from the Player
+
+            if (guess == -1) { // if you not intereset to play then exit
+                System.out.println(" So You Quit the Game Btw that Random no is  " + target);
+                break;
+            }
+
+            attempts++;
+
+            if (guess == target) {
+                System.out.println("Coreect ");
+                System.out.println("You " + attempts + " attempts me guess kiya.");
+                break;
+            } else if (guess < target) {
+                System.out.println("Too low! Try Big Number.");
+                low = guess + 1; // For Hints part if you guess lower value
+            } else {
+                System.out.println("Too high! Try Lower Number");
+                high = guess - 1; // For HInts part if you guess max value
+            }
+
+            // For More Easy to Guess
+            System.out.println("Hint: Number " + low + " aur " + high + " in between");
+        }
+
+        sc.close();
+        System.out.println("Game Over Thank Youuuuuuuuuu Guyssss.....");
+    }
+}
