edbccb8a860b29ff0e5c057b2bb39b1b9c3b1fb3
vash57 <devchaubey03@gmail.com>
2025-08-29 16:17:37 +0530

    Task-2 || Student_Grade_Calculator
    Project Title:
    
    Student Grade Calculator (Java)
    
    Objective:
    
    The main objective of this project is to develop a program that calculates a student’s Total Marks, Average Percentage, and assigns an appropriate Grade based on the marks obtained in different subjects.
    
    Working / Approach:
    
    Input Phase:
    
    The program first asks the user to enter the total number of subjects.
    
    Then, the user inputs the marks (out of 100) for each subject.
    
    Processing Phase:
    
    The program calculates the Total Marks by summing up all the entered marks.
    
    It then computes the Average Percentage using the formula:
    
    Average %
    =
    Total Marks
    Number of Subjects
    Average %=
    Number of Subjects
    Total Marks
    	​
    
    
    Grade Calculation:
    
    A separate function calculateGrade() is used to assign a grade based on the average percentage.
    
    Grading Criteria:
    
    90–100 → A+
    
    80–89 → A
    
    70–79 → B
    
    60–69 → C
    
    50–59 → D
    
    Below 50 → F
    
    Output Phase:
    
    The program displays a Student Report showing:
    
    Total Marks
    
    Average Percentage
    
    Grade

---
+++ Student_Grade_Calculator.java
@@ -0,0 +1,65 @@
+/*
+* PROJECT: STUDENT GRADE CALCULATOR
+* Internship:CODSOFT
+* Task: 2
+* Description: 
+* This program is a for a student grade calulator
+* where we take a no of each subject between (0-100) and after 
+* we calculate the average and Declare thier Grade.......
+* Author: DEV KUMAR CHAUBEY
+* Date : 295 Aug 2025
+*/
+import java.util.Scanner;
+public class Student_Grade_Calculator {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input form the user for no of subjects.
+        System.out.print("Enter the number of subjects: ");
+        int n = sc.nextInt();
+
+        int[] marks = new int[n];
+        int totalMarks = 0;
+
+        // Taking marks input
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            marks[i] = sc.nextInt();
+
+            // Validation (0-100 only)
+            if (marks[i]<0 || marks[i]>100) {
+                System.out.println("Invalid marks! Please enter between 0 and 100.");
+                i--; // repeat for same subject
+                continue;
+            }
+            totalMarks += marks[i];
+        }
+
+        // Calculate average percentage
+        double averagePercentage=(double) totalMarks/n;
+
+        // Grade Define for each student
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        // Result of Stuedent
+        System.out.println("\n----- STUDENT RESULT -----");
+        System.out.println("Total Marks Obtained: " + totalMarks + " / " + (n * 100));
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
