2d4ac82e81a357179418afed3088242532b5c26c
vash57 <devchaubey03@gmail.com>
2025-09-08 23:51:36 +0530

    Task_1_NumberGame.java

--- Task_1_NumberGame.java
+++ Task_1_NumberGame.java
@@ -13,7 +13,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGame {
+public class TASK_1_NumberGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
@@ -61,3 +61,4 @@ public class NumberGame {
     }
 
 }
+
